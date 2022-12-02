a991e0915085268fb6cada4573ada090ce992233
Ming-Jung YU <minayu416@gmail.com>
2022-12-02 18:49:31 +0800

    optimized some style

--- src/components/BlogList.vue
+++ src/components/BlogList.vue
@@ -14,14 +14,14 @@
                 <p class="text-black font-medium">{{ post.date }}</p>
                 <div class="grid grid-cols-2">
                     <div class="text-black">{{ post.title }}</div>
-                    <div class="text-black text-right">{{post.good}} {{ post.comment }}</div>
+                    <!-- <div class="text-black text-right">{{post.good}} {{ post.comment }}</div> -->
                 </div>
             </router-link>
         </div>
         </div>
     </div>
 
-    <div class="flex justify-center text-center gap-2 mb-4 ">
+    <div class="flex justify-center text-center gap-2 mb-4 font-fondamento">
         <div class="hover:text-white cursor-pointer">1</div>
         <div class="hover:text-white cursor-pointer">2</div>
         <div class="hover:text-white cursor-pointer">3</div>
@@ -63,36 +63,36 @@ export default {
         },
         {
             id: '4',
-            title: "vol.1213 Awnswer tour",
-            date: '2022.04.29',
+            title: "vol.1212 Travel Diary",
+            date: '2022.02.19',
             good: '6408',
             comment: "1977"
         },
         {
             id: '5',
-            title: "vol.1213 Awnswer tour",
-            date: '2022.04.29',
+            title: "vol.1211 Job seeking",
+            date: '2021.11.08',
             good: '6408',
             comment: "1977"
         },
         {
             id: '6',
-            title: "vol.1213 Awnswer tour",
-            date: '2022.04.29',
+            title: "vol.1210 Howdy?",
+            date: '2021.11.07',
             good: '6408',
             comment: "1977"
         },
         {
             id: '7',
-            title: "vol.1213 Awnswer tour",
-            date: '2022.04.29',
+            title: "vol.1209 Recent status",
+            date: '2021.07.02',
             good: '6408',
             comment: "1977"
         },
         {
             id: '8',
-            title: "vol.1213 Awnswer tour",
-            date: '2022.04.29',
+            title: "vol.1208 Be happy!",
+            date: '2021.03.11',
             good: '6408',
             comment: "1977"
         }
--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -7,18 +7,18 @@
 
     <div class="px-4 pb-4">
     <div class="grid lg:grid-cols-2 md:grid-cols-2 gap-4">
-        <div class="pb-2 bg-white rounded-lg opacity-75 hover:opacity-100">
+        <div class="pb-2 bg-white rounded-lg hover:opacity-100">
         <img class="brightness-75 object-cover" src="https://minayu0416.files.wordpress.com/2019/10/2019-10-02-14-26-338816725175129355419.jpg" alt="Blog Block Grid"/>
         <p class="pl-2 pt-2 text-black font-medium">2019.10</p>
         <p class="pl-2 text-black font-medium">HANA Restaurant in Alishan.</p>
 
         </div>
-        <div class="pb-2 bg-white rounded-lg opacity-75 hover:opacity-100">
+        <div class="pb-2 bg-white rounded-lg hover:opacity-100">
             <img class="brightness-75 object-cover" src="https://minayu0416.files.wordpress.com/2019/10/2019-09-30-13-29-146638018915226290324.jpg" alt="Blog Block Grid"/>
         <p class="pl-2 pt-2 text-black font-medium">2019.10</p>
         <p class="pl-2 text-black font-medium">Alishan.</p>
         </div>
-        <div class="pb-2 bg-white rounded-lg opacity-75 hover:opacity-100">
+        <div class="pb-2 bg-white rounded-lg hover:opacity-100">
         <img class="brightness-75 object-cover" src="https://minayu0416.files.wordpress.com/2019/02/img_0469-1.jpg" alt="Blog Block Grid"/>
         <p class="pl-2 pt-2 text-black font-medium">2018.02</p>
         <p class="pl-2 text-black font-medium">Iceland.</p>
