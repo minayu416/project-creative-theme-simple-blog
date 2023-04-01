b2de8fc712c5d900de2e3bd5501ba8df891ab7de
Ming-Jung YU <minayu416@gmail.com>
2023-04-01 18:24:50 +0800

    change git name

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/project-creative-theme-simple-blog/',
   plugins: [vue()],
   resolve: {
     alias: {
